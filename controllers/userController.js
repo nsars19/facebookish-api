cd5bc6af1b9ea3a6859170fec509635987ab04a3
Nick Sardonini <nsardonini@gmail.com>
2021-03-17 16:21:15 -0400

    add function to create a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 exports.getUsers = (req, res, next) => {
   User.find({}).then((users) => res.json(users));
@@ -10,4 +11,23 @@ exports.findUser = (req, res, next) => {
   User.findById(userId)
     .populate("posts")
     .then((user) => res.json(user));
+};
+
+exports.newUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    passwordHash,
+  });
+
+  user
+    .save()
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err.message));
 };
