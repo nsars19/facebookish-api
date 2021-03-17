4ad5305efd5c617bca941f065d42e83c0d97065a
Nick Sardonini <nsardonini@gmail.com>
2021-03-17 16:21:02 -0400

    add function to fetch user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,3 +3,11 @@ const User = require("../models/user");
 exports.getUsers = (req, res, next) => {
   User.find({}).then((users) => res.json(users));
 };
+
+exports.findUser = (req, res, next) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .populate("posts")
+    .then((user) => res.json(user));
+};
