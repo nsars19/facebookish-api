2ed4c4dc5b5df0c6a4170128bcd3f8ddf17514f0
Nick Sardonini <nsardonini@gmail.com>
2021-03-18 16:07:23 -0400

    add User Routes & link to controller actions

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", userController.getUsers);
+router.get("/:userId", userController.findUser);
+router.post("/new", userController.newUser);
+router.post("/update/:userId", userController.updateUser);
+router.post("/delete/:userId", userController.deleteUser);
 
 module.exports = router;
