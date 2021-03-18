9ec348b8f41199db1dda7af94775a6357fe380e7
Nick Sardonini <nsardonini@gmail.com>
2021-03-18 16:13:00 -0400

    make User controller methods asynchronous

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,15 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 
 // INDEX
-exports.getUsers = (req, res, next) => {
-  User.find({}).then((users) => res.json(users));
+exports.getUsers = async (req, res, next) => {
+  await User.find({}).then((users) => res.json(users));
 };
 
 // SHOW
-exports.findUser = (req, res, next) => {
+exports.findUser = async (req, res, next) => {
   const { userId } = req.params;
 
-  User.findById(userId)
+  await User.findById(userId)
     .then((user) => res.json(user))
     .catch((err) => res.json({ message: "User not found." }));
 };
