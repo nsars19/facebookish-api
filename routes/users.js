837021e0a3ed26448ea9e9fb16566a20cf961c39
Nick Sardonini <nsardonini@gmail.com>
2021-03-18 20:53:12 -0400

    remove id param from uri, as it is passed in the request body

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/userController");
 router.get("/", userController.getUsers);
 router.get("/:userId", userController.findUser);
 router.post("/new", userController.newUser);
-router.put("/update/:userId", userController.updateUser);
+router.put("/update", userController.updateUser);
 router.delete("/delete/:userId", userController.deleteUser);
 
 module.exports = router;
