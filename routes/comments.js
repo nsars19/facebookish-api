0646160f319c5031a89af1fe3851eb6c6feadee9
Nick Sardonini <nsardonini@gmail.com>
2021-03-22 12:21:48 -0400

    add routes for comments

---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const comment = require("../models/comment");
+const router = express.Router();
+const commentController = require("./../controllers/commentController");
+
+router.get("/bypost/:postId", commentController.getCommentsByPostId);
+router.get("/byuser/:userId", commentController.getCommentsByUserId);
+router.post("/new", commentController.createComment);
+router.put("/update", commentController.updateComment);
+router.delete("delete", commentController.deleteComment);
+
+module.exports = router;
