a955798324660b0094ad1b8ee2c9b4414e10ffe6
Nick Sardonini <nsardonini@gmail.com>
2021-03-22 21:31:04 -0400

    add pending friends to User schema

--- models/user.js
+++ models/user.js
@@ -37,6 +37,7 @@ const UserSchema = new Schema({
   posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  pendingFriends: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 UserSchema.plugin(uniqueValidator);
