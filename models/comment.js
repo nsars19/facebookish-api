130a1bd7fa2be4c9c442e5e3b4bbd5d3d5f9576c
Nick Sardonini <nsardonini@gmail.com>
2021-03-23 15:48:36 -0400

    uncomment out likes in Comment & Post schemas

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,7 @@ const CommentSchema = new Schema({
   text: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   post: { type: Schema.Types.ObjectId, red: "Post", required: true },
-  // likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
+  likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const PostSchema = new Schema({
   text: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  // likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
+  likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
 });
 
 module.exports = mongoose.model("Post", PostSchema);
