3e2ee25466094a362c7236a0871e7a51e8bc54d2
Nick Sardonini <nsardonini@gmail.com>
2021-03-29 15:12:05 -0400

    remove postId from params as it's passed in the request body

--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,7 @@ router.get("/:postId", postController.showPost);
 router.get("/byuser/:userId", postController.getUserPosts);
 router.get("/feed/:userId", postController.getFriendsPosts);
 router.post("/new", postController.createPost);
-router.put("/update/:postId", postController.updatePost);
+router.put("/update", postController.updatePost);
 router.delete("/delete", postController.deletePost);
 
 module.exports = router;
