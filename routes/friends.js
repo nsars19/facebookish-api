e029f3b81618d24c94e039a58b1eb79fa0a8cca8
Nick Sardonini <nsardonini@gmail.com>
2021-04-01 17:43:13 -0400

    add routes for /accept and /deny on the friends router

--- routes/friends.js
+++ routes/friends.js
@@ -5,5 +5,7 @@ const friendController = require("./../controllers/friendController");
 router.get("/pending/:userId", friendController.getPending);
 router.get("/pending-sent/:userId", friendController.getSentRequests);
 router.post("/add", friendController.addFriend);
+router.post("/accept", friendController.acceptRequest);
+router.post("/deny", friendController.denyRequest);
 
 module.exports = router;
