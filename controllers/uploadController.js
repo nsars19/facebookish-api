19cd1aa9fff90c137b911da8b00b5f3e710c1e4b
Nick Sardonini <nsardonini@gmail.com>
2021-04-05 15:29:44 -0400

    add upload router & controller

---
+++ controllers/uploadController.js
@@ -0,0 +1,45 @@
+const Post = require("./../models/post");
+const Photo = require("./../models/photo");
+const photoItem = require("./../utils/storage")();
+const upload = require("./../utils/uploads");
+
+exports.handleUpload = [uploadImage, buildPhoto, buildPost];
+
+async function uploadImage(req, res, next) {
+  upload(req, res, (err) => {
+    if (err) res.sendStatus(500);
+    next();
+  });
+}
+
+// BUILD MONGO DOCUMENT & SAVE
+async function buildPhoto(req, res, next) {
+  const { originalname, destination, filename, path } = req.file;
+
+  const photo = new Photo({
+    originalname,
+    destination,
+    filename,
+    path,
+  });
+
+  photo.save();
+  photoItem.set(photo);
+  next();
+}
+
+// BUILD MONGO DOCUMENT, SAVE, AND HANDLE RESPONSE
+async function buildPost(req, res, next) {
+  const { text } = req.body;
+  const { userId } = req.params;
+
+  const post = new Post({
+    text,
+    author: userId,
+    photo: photoItem.get()._id,
+  });
+
+  post.save();
+  photoItem.set({});
+  res.send({ path: req.file.path });
+}
