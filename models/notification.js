fec168127d70d76b054e06cdfef4697a2806821e
Nick Sardonini <nsardonini@gmail.com>
2021-04-08 20:07:57 -0400

    add createdAt field to notification model

--- models/notification.js
+++ models/notification.js
@@ -6,6 +6,7 @@ const NotificationSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   notificationType: String, // eg. comment/like
   sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Notification", NotificationSchema);
