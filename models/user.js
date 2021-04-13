eeae00652a1a4bdacb8a0630419f78738546182f
Nick Sardonini <nsardonini@gmail.com>
2021-04-13 16:58:44 -0400

    add default banner image

--- models/user.js
+++ models/user.js
@@ -38,6 +38,10 @@ const UserSchema = new Schema({
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
   pendingFriends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  bannerSrc: {
+    type: String,
+    default: "public/images/banner/default.png",
+  },
   profilePhotoSrc: {
     type: String,
     default: "public/images/profile/default.jpg",
