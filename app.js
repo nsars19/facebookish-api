98a983d06a05d998554711f17fdcdd41bbce236f
Nick Sardonini <nsardonini@gmail.com>
2021-04-20 17:29:06 -0400

    change cors settings
    allows access from anywhere

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors());
 
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/", indexRouter);
