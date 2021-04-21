284c23b0f12202757f0ecec28edcf540e64ba988
Nick S <nsardonini@gmail.com>
2021-04-21 11:33:33 -0400

    update env vars

--- s3.js
+++ s3.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const S3 = require("aws-sdk/clients/s3");
 const fs = require("fs");
 
-const bucketName = process.env.AWS_BUCKET_NAME;
+const bucketName = process.env.S3_BUCKET_NAME;
 const region = process.env.AWS_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new S3({
   region,
